9bc3427ae1af98b1255432c3d34db459fde74358
Luizfer-Col <luizfercolorado@gmail.com>
2024-08-26 22:37:36 -0500

    feat: implement text encryption logic and connect script to HTML

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ const conversion = {
   o: "ober",
   u: "ufat",
 };
+document.addEventListener("DOMContentLoaded", function () {
+  const $ = (id) => document.getElementById(id);
+
+  const encryptButton = $("encrypt-button");
+  const textArea = $("input-text");
+  const texResult = $("result-text");
+
+  const encrypter = () => {
+    let string = textArea.value;
+    for (const [vowel, textEncript] of Object.entries(conversion)) {
+      string = string.replaceAll(vowel, textEncript);
+    }
+    texResult.textContent = string;
+    textArea.value = "";
+    return string;
+  };
+
+  encryptButton.addEventListener("click", () => encrypter());
+});
